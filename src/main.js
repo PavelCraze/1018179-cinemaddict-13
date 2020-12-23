8561bd9176e07b2092e2ef304a22f65163dc2040
PavelCraze <54514757+PavelCraze@users.noreply.github.com>
2020-12-23 21:23:26 +0300

    WIP

--- src/main.js
+++ src/main.js
@@ -1,14 +1,35 @@
 import {menuView} from './views/menu-view';
 import {rankView} from './views/rank-view';
-import {cardView} from './views/films-view';
-import {popupView} from './views/popup-view';
+import {filmsView} from './views/films-view';
+import {film} from './views/card-view';
+// import {popupView} from './views/popup-view';
 import {render} from './util';
 
 const headerElement = document.querySelector(`.header`);
 const mainElement = document.querySelector(`.main`);
 
-
 render(headerElement, rankView());
 render(mainElement, menuView());
-render(mainElement, cardView());
-render(mainElement, popupView());
+render(mainElement, filmsView());
+// render(mainElement, popupView());
+
+const filmsElement = document.querySelector(`.films-list__container`);
+const filmsCommentedElements = document.querySelectorAll(`.films-list--commented`);
+for (let i = 0; i < 3; i++) {
+  render(filmsElement, film());
+}
+
+const filmsRatedElements = document.querySelectorAll(`.films-list--rated`);
+for (let i = 0; i < 3; i++) {
+  render(filmsElement, film());
+}
+
+
+filmsCommentedElements.forEach((el) => {
+  render(el.querySelector(`.films-list__container`), film());
+});
+
+filmsRatedElements.forEach((el) => {
+  render(el.querySelector(`.films-list__container`), film());
+});
+
--- src/util.js
+++ src/util.js
@@ -11,4 +11,21 @@ const createElement = (template) => {
   return newElement.firstElementChild;
 };
 
-export {render, createElement};
+const getRandomNumber = (min, max) => min + Math.floor(Math.random() * (max + 1 - min));
+const getRandomElement = (array) => {
+  const rand = Math.floor(Math.random() * array.length);
+  return array[rand];
+};
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+const getShuffledSubarray = (array, numberOfElements) => {
+  const shuffledArr = shuffleArray(array);
+  return shuffledArr.slice(0, numberOfElements);
+};
+
+export {render, createElement, getRandomNumber, getRandomElement, getShuffledSubarray};
